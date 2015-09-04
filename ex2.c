2fa937c51a2288d99396b54850e96c52205d08be
Thayna Maria Morim de Barros Barreto <tmorimbarreto@gmail.com>
2015-09-04 16:50:17 -0300

    adicionado doxygen para preenchimento

--- ex2.c
+++ ex2.c
@@ -1,3 +1,44 @@
+/* ************************************************************************ *
+ *    PROGRAM_NAME, VERSION                                                 *
+ *    BRIEF_DESCRIPTION                                                     *
+ *                                                                          *
+ *    Copyright (C) 2015 by Nome Completo                                   *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by  *
+ *    the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program; if not, write to the                         *
+ *    Free Software Foundation, Inc.,                                       *
+ *    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                          *
+ *    To contact the author, please write to:                               *
+ *    Nome Completo <email@dominio.dd>                                      *
+ *    Webpage: http://www.example.com                                       *
+ *    Phone: +cc (dd) xxxx-xxxx                                             *
+ * ************************************************************************ *
+ * 
+ */
+/**
+ * \file ex2.c
+ * \brief 
+ * \author Your Name Here
+ * \version 
+ * \date 2015-09-04
+ */
+/**
+ * \name 
+ * @{ */
+/**  @} */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
